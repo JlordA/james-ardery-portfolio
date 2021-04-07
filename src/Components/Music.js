9712bb69fa114e8f481195245a6d3e0409b11739
JlordA <james.ardery@gmail.com>
2021-04-07 16:58:13 -0400

    soundcloud player for lushes added

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -1,5 +1,18 @@
 function Music() {
-  return <h1>Hello Music</h1>;
+  return (
+    <div className="music-div">
+      <h1>Hello Music</h1>
+      <iframe
+        width="300"
+        height="475"
+        scrolling="no"
+        frameBorder="no"
+        allow="autoplay"
+        title="LUSHES"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/23341535&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
+    </div>
+  );
 }
 
 export default Music;
