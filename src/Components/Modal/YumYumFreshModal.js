4557ccb7258fe14daa56278e5d2fbd13e5344b68
JlordA <james.ardery@gmail.com>
2021-05-18 12:41:52 -0400

    YumYum Modal addition part 1

---
+++ src/Components/Modal/YumYumFreshModal.js
@@ -0,0 +1,46 @@
+import "../../css/modal.css";
+
+function YumYumModal(props) {
+  const clickHandler = () => {
+    props.toggleState();
+  };
+
+  return (
+    <div className="modalInner">
+      <div className="modalImage">
+        <iframe
+          width="500"
+          height="400"
+          src="https://www.youtube.com/embed/vBt_O-vvY1o"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="modalText">
+        <h1>Yum Yum Fresh</h1>
+        <p>
+          Yum Yum Fresh is a consumer app designed to show users local seasonal
+          produce based on the state they are located in and the current month.
+          The app is built with a React frontend using hooks for state
+          management. Bootstrap and vanilla css are used for styling. The
+          backend API is built in Ruby on Rails. The backend features custom
+          serializers and a PostgresQL database.
+        </p>
+        <form
+          action="https://yumyumfresh.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <input className="modalButton" type="submit" value="Visit the Site" />
+        </form>
+        <button className="modal-exit" onClick={clickHandler}>
+          X
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default YumYumModal;
--- src/Components/Tech.js
+++ src/Components/Tech.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import YumYumFreshModal from "./Modal/YumYumFreshModal"
 import RollorHeroModal from "./Modal/RollorHeroModal";
 import PettoModal from "./Modal/PettoModal";
 import JokesOnUsModal from "./Modal/JokesOnUsModal";
-import "../css/tech.css"
+import "../css/tech.css";
 
 function Tech() {
+  const [yumModalState, setYumYumModalState] = useState(false);
   const [rollModalState, setRollModalState] = useState(false);
   const [pettoModalState, setPettoModalState] = useState(false);
   const [jokeModalState, setJokeModalState] = useState(false);
+
+  const toggleYumYumModal = () => {
+    setYumYumModalState(!yumModalState);
+  };
 
   const toggleRollorHeroModal = () => {
     setRollModalState(!rollModalState);
@@ -27,6 +33,9 @@ function Tech() {
         <h1>RECENT PROJECTS</h1>
       </div>
       <div className="tech-modal-div">
+        <div className={`modalBackground modalShowing-${yumModalState}`}>
+          <YumYumFreshModal toggleState={toggleYumYumModal} />
+        </div>
         <div className={`modalBackground modalShowing-${rollModalState}`}>
           <RollorHeroModal toggleState={toggleRollorHeroModal} />
         </div>
@@ -39,37 +48,45 @@ function Tech() {
         <div className="app-img-div">
           <div className="app-container">
             <img
+              onClick={() => toggleYumYumModal()}
+              value={yumModalState}
+              style={{ width: 650}}
+              className="app-img"
+              src="image/YumYumFresh.png"
+              alt="rollorhero"
+            />
+            <p>Yum Yum Fresh - React, Hooks, Rails, Bootstrap</p>
+          </div>
+          <div className="app-container">
+            <img
               onClick={() => toggleRollorHeroModal()}
               value={rollModalState}
+              style={{ width: 650}}
               className="app-img"
               src="image/rollorhero.png"
               alt="rollorhero"
             />
-            <p>
-              Roll or Hero - React, Redux, Rails
-            </p>
+            <p>Roll or Hero - React, Redux, Rails</p>
           </div>
           <div className="app-container">
             <img
               onClick={() => togglePettoModal()}
+              style={{ width: 650}}
               className="app-img"
               src="image/petto.png"
               alt="petto"
             />
-            <p>
-              Petto - JavaScript, Rails
-            </p>
+            <p>Petto - JavaScript, Rails</p>
           </div>
           <div className="app-container">
             <img
               onClick={() => toggleJokeModal()}
+              style={{ width: 650}}
               className="app-img"
               src="image/JokesonUs.png"
               alt="jokesonus"
             />
-            <p>
-              Jokes On Us - Rails
-            </p>
+            <p>Jokes On Us - Rails</p>
           </div>
         </div>
       </div>
